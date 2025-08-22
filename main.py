ca71dfc966018f6b800e3b7e987f212f3ef3ce2c
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2025-08-22 10:56:51 -0300

    Downloads hekate

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ package = Path("package")
 rmtree(package, ignore_errors=True)
 package.mkdir()
 
-def get(url):
-    with urlopen(Request(url)) as response:
-        return loads(response.read().decode("utf-8"))
+def get(uri):
+    with urlopen(Request(uri)) as response:
+        return response.read()
 
-releases = get("https://api.github.com/repos/ctcaer/hekate/releases")
-assets = get(f"https://api.github.com/repos/ctcaer/hekate/releases/{releases[0]["id"]}/assets")
+def get_json(uri):
+    return loads(get(uri).decode("utf-8"))
+
+def download(uri, dest_folder):
+    filename = uri.split("/")[-1]
+    dest_path = dest_folder / filename
+    open(dest_path, "wb").write(get(uri))
+
+releases = get_json("https://api.github.com/repos/ctcaer/hekate/releases")
+assets = get_json(f"https://api.github.com/repos/ctcaer/hekate/releases/{releases[0]["id"]}/assets")
 
 url = [a for a in assets if a["name"].startswith("hekate")][0]["browser_download_url"]
 
-del assets, releases
+download(url, package)
+
+del url, assets, releases
 
 pass
